16302bc8ac164b837bd88a6246b54ef4f1cf6987
Bill Smargiassi <bill@smargiassi.us>
2017-06-27 17:16:18 -0400

    Initial commit
    Still learning how to use this for searching

---
+++ zd.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+
+require "zendesk_api"
+require "string-scrub"
+require "json"
+
+def usage
+	puts "Usage: zd.rb »command«"
+	puts "Command can be 'search'"
+end
+
+def search(client, terms)
+	search_terms = terms.join(' ')
+	output = client.search(:query => search_terms)
+	output.each { |elt| jj elt }
+end
+
+jconfig = JSON.parse(File.read(ENV['HOME'] + "/.zdapi.json"))
+
+client = ZendeskAPI::Client.new do |config|
+	config.url = jconfig["url"]
+	config.username = jconfig["username"]
+	config.token = jconfig["token"]
+	config.retry = true
+end
+
+command = ARGV.shift
+
+# Process commands
+case command
+	when "search" 
+		search(client, ARGV)
+	else
+		usage()
+end
